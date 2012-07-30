b73b51fa85e75c1ae4955cb12cfc9e47bc643752
Mike Boyle <mboyle@backstopsolutions.com>
2012-07-29 19:16:40 -0500

    Add scala solution to problem 2

---
+++ scala/0002.scala
@@ -0,0 +1,5 @@
+val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(
+     fibs.tail).map(n => n._1 + n._2)
+
+val result = fibs.view.takeWhile(_ < 4000000).filter(_ % 2 == 0).sum
+println(result)
