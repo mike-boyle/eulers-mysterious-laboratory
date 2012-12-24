37c35eb0fe895083a6113561e2f5dff0528a3fb7
Mike Boyle <mboyle@backstopsolutions.com>
2012-12-24 14:16:14 -0600

    Refactor 9 slightly

--- scala/src/Euler0009.scala
+++ scala/src/Euler0009.scala
@@ -2,13 +2,13 @@ object Euler0009 {
   def main(args: Array[String]) = {
     val limit = (1 to 1000) find { x => x + math.sqrt(x) > 1000 } get
 
-    for {
+    val result = for {
       a <- (1 to limit)
       b <- (a to limit)
       c = 1000 - a - b
       if a * a + b * b == c * c
-    } {
-      println(a * b * c)
-    }
+    } yield a * b * c
+
+    println(result.head)
   }
 }
